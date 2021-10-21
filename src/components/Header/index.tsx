af4cf897838bd3201c404fa9aaf311ba5442d5d3
Oseas Moreto <oseas_moreto@hotmail.com>
2021-10-21 14:42:40 -0300

    Adiciona metodo de logout

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,23 +6,28 @@ import {
   TouchableOpacity
 } from 'react-native';
 
+import { useAuth } from '../../hooks/auth';
+
 import { styles } from './styles';
 
 import LogoSVG from '../../assets/logo.svg';
 import { UserPhoto } from '../UserPhoto';
 
 export function Header(){
+  const {user, signOut} = useAuth();
   return (
     <View style={styles.container}>
       <LogoSVG />
 
       <View style={styles.logoutButton}>
-        <TouchableOpacity>
-          <Text style={styles.logoutText}>Sair</Text>
+        <TouchableOpacity
+        onPress={signOut}
+        >
+          {user && <Text style={styles.logoutText}>Sair</Text>}
         </TouchableOpacity>
 
         <UserPhoto
-        imageUri='https://github.com/oseasmoreto.png'
+        imageUri={user?.avatar_url}
         />
       </View>
     </View>
